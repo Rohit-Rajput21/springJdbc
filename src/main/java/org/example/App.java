fce6b0946403bc9cb3dee505a2c441f74800a9ef
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-02-05 13:53:47 +0530

    Initial commit: SpringJDBC insert query

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+//        System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate jdbcTemplate =context.getBean("jdbcTemplate", JdbcTemplate.class);
+        String query="insert into student(id,name,city) values(?,?,?)";
+        int result= jdbcTemplate.update(query,112,"Manish1 ","Indore");
+        System.out.println("Rows affected...."+result);
+    }
+}
