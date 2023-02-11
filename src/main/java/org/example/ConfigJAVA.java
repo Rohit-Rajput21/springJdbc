c21afbcb30e1956ad01973cf3b4416470ed84ff5
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-02-12 01:32:44 +0530

    commit:Configuration using AutoWired Annotation

--- src/main/java/org/example/ConfigJAVA.java
+++ src/main/java/org/example/ConfigJAVA.java
@@ -3,6 +3,7 @@ package org.example;
 import org.StudentDao.StudentDao;
 import org.StudentDao.StudentDaoImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -10,6 +11,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages ="org.StudentDao")
 public class ConfigJAVA {
     @Bean(name={"ds"})
     public DataSource getDataSource(){
@@ -26,10 +28,5 @@ public class ConfigJAVA {
         jdbcTemplate.setDataSource(getDataSource());
         return jdbcTemplate;
     }
-    @Bean(name={"studentDao"})
-    public StudentDao getStudentDao(){
-        StudentDaoImpl studentDao=new StudentDaoImpl();
-        studentDao.setJdbcTemplate(getTemplate());
-        return studentDao;
-    }
+
 }
